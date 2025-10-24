f68f9ba5179958abcbe2666e98dce92228c3e214
Gustavo Firme <gffiorot@outlook.com>
2025-10-24 10:17:51 -0300

    Aula 24/10, teve java

---
+++ testes2HTML/script.js
@@ -0,0 +1,16 @@
+
+function verificacao(){
+    let nomeInput = document.getElementById('name');
+    let idadeInput = document.getElementById('idade');
+    let mensagemResultado = document.getElementById('resultado');
+
+    let nome = nomeInput.value.trim(); //trim() tira os espaços
+    let idade = parseInt(idadeInput.value);
+
+    let vereador = 18;
+    let prefeitoDeputadoJuizPaz = 21;
+    let governador = 30;
+    let senadorPresidente = 35;
+
+    
+}
